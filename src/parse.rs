6ac7f6b810264d9d023d1ebaddca211dcbb76186
nanonicholson <nanonicholson@gmail.com>
2019-11-05 20:27:48 +0600

    Add map & map_err in parse trait

--- src/parse.rs
+++ src/parse.rs
@@ -5,6 +5,20 @@ pub trait Parse<I> {
     type Out;
 
     fn parse(&self, input: I) -> Result<(Self::Out, I), Self::Err>;
+
+    fn map<F>(self, f: F) -> Map<Self, F>
+        where
+            Self: Sized,
+    {
+        Map(self, f)
+    }
+
+    fn map_err<F>(self, f: F) -> MapErr<Self, F>
+        where
+            Self: Sized,
+    {
+        MapErr(self, f)
+    }
 }
 
 impl<'i> Parse<&'i str> for str {
@@ -119,9 +133,40 @@ impl<P1, P2, I, R1, R2, E> Parse<I> for (P1, P2)
     }
 }
 
+pub struct Map<P, F>(P, F);
+
+impl<P, F, A, B, I> Parse<I> for Map<P, F>
+    where
+        P: Parse<I, Out=A>,
+        F: Fn(A) -> B,
+{
+    type Err = P::Err;
+    type Out = B;
+
+    fn parse(&self, input: I) -> Result<(Self::Out, I), Self::Err> {
+        self.0.parse(input).map(|(out, rest)| ((self.1)(out), rest))
+    }
+}
+
+pub struct MapErr<P, F>(P, F);
+
+impl<P, F, E, G, I> Parse<I> for MapErr<P, F>
+    where
+        P: Parse<I, Err=E>,
+        F: Fn(E) -> G + Copy,
+{
+    type Err = G;
+    type Out = P::Out;
+
+    fn parse(&self, input: I) -> Result<(Self::Out, I), Self::Err> {
+        self.0.parse(input).map_err(self.1)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::parser;
 
     #[test]
     fn parse_str() {
@@ -178,5 +223,21 @@ mod tests {
         assert_eq!(l.parse("b"), Ok(("b", "")));
         assert_eq!(l.parse("c"), Ok(("c", "")));
         assert_eq!(l.parse("d"), Err(()));
+    }
+
+    #[test]
+    fn parse_map() {
+        let l = parser("a").map(|_| 1);
+
+        assert_eq!(l.parse("ab"), Ok((1, "b")));
+        assert_eq!(l.parse("c"), Err(()));
+    }
+
+    #[test]
+    fn parse_map_err() {
+        let l = parser("a").map_err(|_| 1);
+
+        assert_eq!(l.parse("ab"), Ok(("a", "b")));
+        assert_eq!(l.parse("c"), Err(1));
     }
 }
